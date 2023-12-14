c0531870928f31d629f3ec982aab16d2977fa2ff
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-14 13:02:26 +0530

    Terraform - create parameter store

--- main.tf
+++ main.tf
@@ -7,10 +7,8 @@ terraform {
 }
 
 variable "parameters" {
-  default = [
-    { name = "test", type = "string", value = "test"},
-    { name = "test1", type = "string", value = "test1"}
-  ]
+  default =
+    { name = "test", type = "string", value = "test"}
 }
 resource "aws_ssm_parameter" "params" {
   count = length(var.parameters)
