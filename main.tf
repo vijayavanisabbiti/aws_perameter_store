561cb2e7ae59358f77513e5959b3862a9aa1347c
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-14 12:55:38 +0530

    Terraform - create parameter store

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "vijayavani-terraform-state"
+    key    = "test/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "parameters" {
+  default = [
+    { name = "test", type = "string", value = "test"},
+    { name = "test1", type = "string", value = "test1"}
+  ]
+}
+resource "aws_ssm_parameter" "params" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
