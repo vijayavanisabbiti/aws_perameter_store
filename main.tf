9fa8934d30a89f983b5825c8b17c0729b85c5c72
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-14 13:37:18 +0530

    Terraform - create parameter store

--- main.tf
+++ main.tf
@@ -3,12 +3,13 @@ terraform {
     bucket = "vijayavani-terraform-state"
     key    = "test/terraform.tfstate"
     region = "us-east-1"
-    overwrite = true
+
   }
 }
 
 resource "aws_ssm_parameter" "params" {
   count = length(var.parameters)
+  overwrite = true
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
