d9e67aa425ee69c70e8b77735492f43d4e4a2988
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-22 17:13:28 +0530

    Terraform - App load balancer creation

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "parameters" {
   default = [
-    { name = "backend.dev.DB_HOST", type = "String", value = "mysql-dev.vijayavanimanju.online" },
+    { name = "backend.dev.DB_HOST", type = "String", value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
     { name = "backend.dev.DB_USER", type = "String", value = "centos" },
     { name = "frontend.dev.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.vijayavanimanju.online:8080/" },
 
