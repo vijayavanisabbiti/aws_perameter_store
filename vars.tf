b43a462a81eab6d4bf6c4204da3a9b18d0150b0e
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-22 18:38:35 +0530

    Terraform - App load balancer creation

--- vars.tf
+++ vars.tf
@@ -5,13 +5,13 @@ variable "parameters" {
     { name = "frontend.dev.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.vijayavanimanju.online:8080/" },
 
     ## RDS
-    { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
+    { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.c21qj8xqyfw1.us-east-1.rds.amazonaws.com" },
     { name = "dev.rds.username", type = "String", value = "admin1" },
 
 
     ### Passwords.
     # Passwords should never be kept under code, It always been created mostly manually / saperate automation where code is not involved.
     { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
-    { name = "backend.dev.DB_PASS", type = "String", value = "DevOps321" }
+
   ]
 }
