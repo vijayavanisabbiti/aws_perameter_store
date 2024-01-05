2f14cd4094df0160be0b53cf34002b7f6d233dc3
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-05 16:49:14 +0530

    Golden AMI instance creation

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "parameters" {
   default = [
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.c21qj8xqyfw1.us-east-1.rds.amazonaws.com" },
     { name = "frontend.dev.BACKEND_ENDPOINT", type = "String", value = "http://internal-dev-backend-56968189.us-east-1.elb.amazonaws.com/" },
+    { name = "ssh.username", type = "String", value = "root"},
 
 
     ## RDS
@@ -13,6 +14,7 @@ variable "parameters" {
     # Passwords should never be kept under code, It always been created mostly manually / saperate automation where code is not involved.
     { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
     { name = "sonarqube.token", type = "SecureString", value = "5ab20fcf75bb8d523de99176f1a46ee86871ff11"},
-    { name = "art.password", type = "SecureString", value = "Admin1234"}
+    { name = "art.password", type = "SecureString", value = "Admin1234"},
+    { name = "ssh.password", type = "SecureString", value = "DevOps321"}
   ]
 }
